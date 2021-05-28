7e1ecf1d894ffcf061a02c58b16ac4fd3f75348c
Pradnesh Kalkar <ppk.kalkar@gmail.com>
2021-05-28 13:18:36 +0530

    Add logout and register views

--- gmail/views.py
+++ gmail/views.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
 
 # -------------- Views definition --------------- #
 
@@ -42,3 +44,37 @@ def login_view(request):
     # if request.method == "GET"
     else:
         return render(request, "gmail/login.html")
+
+
+@csrf_exempt
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+
+        # Ensure that the password matches confirmation
+        if password != confirmation:
+            return render(request, "gmail/register.html", {
+                "message": "Error: Passwords don't match!"
+            })
+        
+        # Attempt to create a new user
+        try:
+            user = User.objects.create_user(email, email, password) # the username is equal to email here
+            user.save()
+        except IntegrityError as e:
+            return render(request, "gmail/register.html", {
+                "message": "Error: Email already taken!"
+            })
+
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+
+    else: 
+        return render(request, "gmail/register.html")
