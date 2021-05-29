9ec8b5f93acaa20ab434d58de773881953682f14
Pradnesh Kalkar <ppk.kalkar@gmail.com>
2021-05-29 17:17:20 +0530

    Create email related views in backend

--- gmail/urls.py
+++ gmail/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     # authentication
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+
+    # email handling
+    path("emails", views.compose, name="compose"),
+    path("emails/<int:email_id>", views.email, name="email"),
+    path("emails/<str:mailbox>", views.mailbox, name="mailbox")
 ]
